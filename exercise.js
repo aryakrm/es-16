cd79de144d8d33db69f7860aca06a55a670feea5
Arya Karimi <devaryakrm@gmal.com>
2022-09-16 23:32:56 +0300

    code added

---
+++ exercise.js
@@ -0,0 +1,7 @@
+let isSunnyDay = true;
+let isHomeworkCompleted = false;
+let possible =
+  isSunnyDay && isHomeworkCompleted == true
+    ? "Jesse can go out to play"
+    : "Jesse stays home";
+console.log(possible);
